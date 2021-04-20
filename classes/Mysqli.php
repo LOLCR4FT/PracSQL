9841c7b52b65b374cb8d3e4de1614d5d8d04af6a
Javier Perez <jperez7@milaifontanals.org>
2021-04-20 16:46:36 +0200

    Llibre working with MYSQLI

--- classes/Mysqli.php
+++ classes/Mysqli.php
@@ -45,7 +45,7 @@ class Mysqli2
         foreach ($field_info as $field) {
             echo "<td>" . $field->name . "</td>";
         }
-        echo "<td>Elimina</td><td>Edita</td>";
+        echo "<td>Elimina</td><td>Edita</td><td>Filtra</td>";
         echo "</tr>";
 
         while ($row = $result->fetch_row()) {
@@ -53,7 +53,9 @@ class Mysqli2
             for ($i = 0; $i < count($field_info); $i++) {
                 echo "<td>" . $row[$i] . "</td>";
             }
-            echo "<td><a href='http://localhost/conectors?used=" . $this->num . "&dA=" . $row[0] . "'>Elimina</a></td><td><a href='http://localhost/conectors?used=" . $this->num . "&eA=" . $row[0] . "'>Editar</a></td>";
+            echo "<td><a href='http://localhost/conectors?used=" . $this->num . "&dA=" . $row[0] . "'>Elimina</a></td><td><a href='http://localhost/conectors?used=" . $this->num . "&eA=" . $row[0] . "'>Editar</a></td>
+            <td><a href='http://localhost/conectors?used=" . $this->num . "&autor=" . $row[0] . "'>Filtra</a></td>
+            ";
             echo "</tr>";
         }
         echo "</table>";
@@ -86,6 +88,111 @@ class Mysqli2
         $stmt = $this->con->prepare("insert into autor(nom,data_naix) values(?,?)");
 
         $stmt->bind_param("ss", $data['nom'], $data['data_naix']);
+        $stmt->execute();
+    }
+
+    function showLlibres()
+    {
+        $result = $this->con->query("select * from llibre");
+        $field_info = $result->fetch_fields();
+        echo "<table border='1'>";
+        echo "<tr>";
+        foreach ($field_info as $field) {
+            echo "<td>" . $field->name . "</td>";
+        }
+        echo "<td>Elimina</td><td>Edita</td>";
+        echo "</tr>";
+
+        while ($row = $result->fetch_row()) {
+            echo "<tr>";
+            for ($i = 0; $i < count($field_info); $i++) {
+                echo "<td>" . $row[$i] . "</td>";
+            }
+            echo "<td><a href='http://localhost/conectors?used=" . $this->num . "&dL=" . $row[0] . "'>Elimina</a></td><td><a href='http://localhost/conectors?used=" . $this->num . "&eL=" . $row[0] . "'>Editar</a></td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+    }
+
+    function selectAutor($autor)
+    {
+        echo "<select name='autors'>";
+        $result = $this->con->query("select id,nom from autor");
+        while ($row = $result->fetch_row()) {
+            if ($row[0] == $autor) {
+                echo "<option value='" . $row[0] . "' selected>" . $row[1] . "</option>";
+            } else {
+                echo "<option value='" . $row[0] . "'>" . $row[1] . "</option>";
+            }
+        }
+        echo "</select>";
+    }
+
+    function getLlibre($id)
+    {
+        if ($stmt = $this->con->prepare("SELECT id_autor,titol,any FROM llibre WHERE id=?")) {
+
+            /* ligar parámetros para marcadores */
+            $stmt->bind_param("i", $id);
+
+            /* ejecutar la consulta */
+            $stmt->execute();
+
+            /* ligar variables de resultado */
+            $stmt->bind_result($col1, $col2, $col3);
+
+            /* obtener valor */
+            $stmt->fetch();
+
+            /* cerrar sentencia */
+            $stmt->close();
+
+            $result = array(
+                'autor' => $col1,
+                'titol' => $col2,
+                'any' => $col3
+            );
+
+            return $result;
+        }
+    }
+
+    function showLLibreAutor($id)
+    {
+        if ($id) {
+            $stmt = $this->con->prepare("SELECT * FROM llibre where id_autor=?");
+            /* ligar parámetros para marcadores */
+            $stmt->bind_param("i", $id);
+            /* ejecutar la consulta */
+            $stmt->execute();
+            /* ligar variables de resultado */
+            $result = $stmt->get_result();
+            echo "<table border='1'>";
+            echo "<tr><td>id</td><td>id_autor</td><td>titol</td><td>any</td><td>Elimina</td><td>Edita</td></tr>";
+            while ($row = $result->fetch_row()) {
+                echo "<tr><td>" . $row[0] . "</td><td>" . $row[1] . "</td><td>" . $row[2] . "</td><td>" . $row[3] . "</td><td><a href='http://localhost/conectors?used=" . $this->num . "&dL=" . $row[0] . "'>Elimina</a></td><td><a href='http://localhost/conectors?used=" . $this->num . "&eL=" . $row[0] . "'>Editar</a></td></tr>";
+            }
+            echo "<tr><td colspan='6' class='centrar'><a href='http://localhost/conectors/?used=" . $this->num . "'>Reset</a></td></tr>";
+            echo "</table>";
+
+            /* cerrar sentencia */
+            $stmt->close();
+        }
+    }
+
+    function insertLlibre($data)
+    {
+        $stmt = $this->con->prepare("insert into llibre(id_autor,titol,any) values(?,?,?)");
+
+        $stmt->bind_param("isi", $data['id_autor'], $data['titol'], $data['any']);
+        $stmt->execute();
+    }
+
+    function editLlibre($data)
+    {
+        $stmt = $this->con->prepare("update llibre set id_autor=?, titol=?, any=? where id=?");
+
+        $stmt->bind_param("ssii", $data['id_autor'], $data['titol'], $data['any'], $data['id']);
         $stmt->execute();
     }
 }
--- index.php
+++ index.php
@@ -1,110 +1,175 @@
-<?php
-include 'classes/Mysqli.php';
-include 'classes/PDO.php';
-include 'classes/Adodb.php';
-include 'classes/Odbc.php';
-include 'classes/SQLServer.php';
-if(isset($_GET['used'])){
-    $selected = $_GET['used'];
-}
+<html>
 
-$boto = 'insereix';
+<head>
+    <title>title</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <link rel="stylesheet" type="text/css" href="css/estil.css" />
+</head>
 
+<body>
+    <?php
+    include 'classes/Mysqli.php';
+    include 'classes/PDO.php';
+    include 'classes/Adodb.php';
+    include 'classes/Odbc.php';
+    include 'classes/SQLServer.php';
+    if (isset($_GET['used'])) {
+        $selected = $_GET['used'];
+    }
 
-if (isset($_POST['send'])) {
-    $selected = $_POST['objects'];
-    header('Location: http://localhost/conectors?used=' . $selected);
-}
-$possible_connections = array('Mysqli', 'Adodb', 'Pdo', 'SQLServer');
+    $boto = 'insereix';
+    $boto2 = 'insereix Llibre';
+    $autor = null;
+    $titol = '';
+    $any = '';
 
-echo "<h3>Selecciona una connexió de BD</h3>";
-echo '<form method="post" name="f1">
+    if (isset($_POST['send'])) {
+        $selected = $_POST['objects'];
+        header('Location: http://localhost/conectors?used=' . $selected);
+    }
+    $possible_connections = array('Mysqli', 'Adodb', 'Pdo', 'SQLServer');
+
+    echo "<h3>Selecciona una connexió de BD</h3>";
+    echo '<form method="post" name="f1">
     <select name="objects">';
 
-for ($i = 0; $i < count($possible_connections); $i++) {
-    if ($i == $selected) {
-        echo '<option value="' . $i . '" selected>' . $possible_connections[$i] . '</option>';
-        continue;
+    for ($i = 0; $i < count($possible_connections); $i++) {
+        if ($i == $selected) {
+            echo '<option value="' . $i . '" selected>' . $possible_connections[$i] . '</option>';
+            continue;
+        }
+        echo '<option value="' . $i . '">' . $possible_connections[$i] . '</option>';
     }
-    echo '<option value="' . $i . '">' . $possible_connections[$i] . '</option>';
-}
 
-echo '</select><br><br>
+    echo '</select><br><br>
     <input type="submit" name="send" value="Selecciona"/>
 </form><hr>';
 
-//en cas de que seleccioni metode mostrar la parafernalia
+    //en cas de que seleccioni metode mostrar la parafernalia
 
-if (isset($_GET['used'])) {
-    $nom = '';
-    $dNaix = '';
+    if (isset($_GET['used'])) {
+        $nom = '';
+        $dNaix = '';
 
-    $selected = $_GET['used'];
-    switch ($selected) {
-        case 0:
-            $obj = new Mysqli2();
-            break;
-        case 1:
-            $obj = new Adodb2();
-            break;
-        case 2:
-            $obj = new PDO2();
-            break;
-        case 3:
-            $obj = new sqlsrv();
-            break;
-        default:
-            header("Refresh:0");
-    }
+        $selected = $_GET['used'];
+        switch ($selected) {
+            case 0:
+                $obj = new Mysqli2();
+                break;
+            case 1:
+                $obj = new Adodb2();
+                break;
+            case 2:
+                $obj = new PDO2();
+                break;
+            case 3:
+                $obj = new sqlsrv();
+                break;
+            default:
+                header("Refresh:0");
+        }
 
-    if (isset($_GET['dA'])) {
-        $obj->deleteAutor($_GET['dA']);
-        header('Location: http://localhost/conectors?used=' . $selected);
-    }
-
-    if (isset($_GET['eA'])) {
-
-        $dades = $obj->getOne($_GET['eA']);
-        $boto = 'edita';
-        if ($dades['nom'] != null && $dades['data_naix'] != null) {
-            $nom = $dades['nom'];
-            $dNaix = $dades['data_naix'];
-        } else {
+        if (isset($_GET['dA'])) {
+            $obj->deleteAutor($_GET['dA']);
             header('Location: http://localhost/conectors?used=' . $selected);
         }
-    }
 
-    if (isset($_POST['ins'])) {
         if (isset($_GET['eA'])) {
-            $data = array(
-                'id' => $_GET['eA'],
-                'nom' => $_POST['nom'],
-                'data_naix' => $_POST['dnaix']
-            );
 
-            $obj->editAutor($data);
-        } else {
-            $data = array(
-                'nom' => $_POST['nom'],
-                'data_naix' => $_POST['dnaix']
-            );
-            $obj->insertAutor($data);
+            $dades = $obj->getOne($_GET['eA']);
+            $boto = 'edita';
+            if ($dades['nom'] != null && $dades['data_naix'] != null) {
+                $nom = $dades['nom'];
+                $dNaix = $dades['data_naix'];
+            } else {
+                header('Location: http://localhost/conectors?used=' . $selected);
+            }
         }
 
-        header('Location: http://localhost/conectors?used=' . $selected);
-    }
+        if (isset($_POST['ins2'])) {
+            if (!isset($_GET['eL'])) {
+                $data = array(
+                    'id_autor' => $_POST['autors'],
+                    'titol' => $_POST['titol'],
+                    'any' => $_POST['any']
+                );
+                $obj->insertLlibre($data);
+            } else {
+                $data = array(
+                    'id' => $_GET['eL'],
+                    'id_autor' => $_POST['autors'],
+                    'titol' => $_POST['titol'],
+                    'any' => $_POST['any']
+                );
+                $obj->editLlibre($data);
+            }
+            header('Location: http://localhost/conectors?used=' . $selected);
+        }
 
+        if (isset($_GET['eL'])) {
+            $dades = $obj->getLlibre($_GET['eL']);
 
-    //dibuixar una taula
-    echo "<h2>Taula autors - SGBD utilitzat: " . $possible_connections[$_GET['used']] . " </h2>";
-    $obj->showAutors();
+            $autor = $dades['autor'];
+            $titol = $dades['titol'];
+            $any = $dades['any'];
 
-    echo '<hr>
-    <h2>Inserció / Edició d\'autor</h2>
-    <form method="post" action="#" name="f2">
-        Nom: <input type="text" value="' . $nom . '" name="nom" /><br>
-        Data Naixement: <input type="text" value="' . $dNaix . '" name="dnaix" /><br>
-        <input type="submit" value="' . $boto . '" name="ins" />
-    </form>    
+            $boto2 = 'Edita';
+        }
+
+        if (isset($_POST['ins'])) {
+            if (isset($_GET['eA'])) {
+                $data = array(
+                    'id' => $_GET['eA'],
+                    'nom' => $_POST['nom'],
+                    'data_naix' => $_POST['dnaix']
+                );
+
+                $obj->editAutor($data);
+            } else {
+                $data = array(
+                    'nom' => $_POST['nom'],
+                    'data_naix' => $_POST['dnaix']
+                );
+                $obj->insertAutor($data);
+            }
+
+            header('Location: http://localhost/conectors?used=' . $selected);
+        }
+
+        //dibuixar una taula
+        echo "<h2 class='centrar'>Taula autors - SGBD utilitzat: " . $possible_connections[$_GET['used']] . " </h2>";
+        echo "<div class='info-container'>";
+        $obj->showAutors();
+        if (!isset($_GET["autor"])) {
+            $obj->showLlibres();
+        } else {
+            $obj->showLLibreAutor($_GET["autor"]);
+        }
+        echo "</div>";
+        echo '<div class="insert-container">
+        <div>
+            <h2>Inserció / Edició d\'autor</h2>
+            <form method="post" action="#" name="f2">
+                Nom: <input type="text" value="' . $nom . '" name="nom" /><br>
+                Data Naixement: <input type="text" value="' . $dNaix . '" name="dnaix" /><br>
+                <input type="submit" value="' . $boto . '" name="ins" />
+            </form> 
+        </div>   
+        <div>
+            <h2>Inserció / Edició de llibres</h2>
+            <form method="post" action="#" name="f2">
+            Autor: ';
+        $obj->selectAutor($autor);
+        echo '<br>
+                Titol: <input type="text" value="' . $titol . '" name="titol" /><br>
+                Any publicació: <input type="text" value="' . $any . '" name="any" /><br>
+                <input type="submit" value="' . $boto2 . '" name="ins2"/>
+            </form> 
+        </div>
+    </div>
     ';
-}
+    }
+    ?>
+</body>
+
+</html>
